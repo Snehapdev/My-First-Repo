8f3e46b476978f51bdbe4c9906bade53526e2c80
Sneha <snehapdev@gmail.com>
2020-04-02 15:45:16 +0530

    initial commit

---
+++ DemoToDoApp/src/main/java/com/application/TODO/DemoToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.application.TODO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoToDoAppApplication.class, args);
+	}
+
+}
---
+++ DemoToDoApp/src/test/java/com/application/TODO/DemoToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.TODO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
